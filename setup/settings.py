77a643a9d2503840b287aea9ad0218910ed485c0
Tiago Mazzon <tiago.mazzon@gmail.com>
2022-11-25 08:26:08 -0300

    fixing media setting

--- setup/settings.py
+++ setup/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'imagens')
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
